c8154fd09e25aeb3cda56193a16246b31578d959
Azadhossain288 <azadhossain016288@gmail.com>
2026-04-16 21:43:43 +0600

    installed daisyui and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-10">
+      
+      <button class="btn btn-info">Info</button>
+      
+    
+      <div className="mt-5 p-5 bg-yellow-400 font-bold">
+        Tailwind Check
+      </div>
+    </div>
+  )
+}
+
+export default App
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Ei line-ti thaka chai
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), 
+  ],
 })
